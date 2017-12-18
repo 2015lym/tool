5464cee519176bcbec0dffe24edbbc27e3b6a091
2015lym <18622797149@163.com>
2017-12-18 10:15:57 +0800

    更新

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { ToastService } from '../../app/services/toast.service';
 
 @Component({
   selector: 'page-home',
@@ -10,16 +13,53 @@ export class HomePage {
 
   private items: Array<string> = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    private toast: ToastService,
+    private storage: Storage) {
 
   }
 
   ionViewDidEnter() {
-    this.items = ['选项一', '选项二', '选项三', '选项四', '选项五'];
+    this.storage.get('draw').then((data) => {
+      if (data) {
+        this.items = data;
+      } else {
+        this.toast.show('没有数据');
+      }
+    });
   }
 
   itemSelected(item: string) {
-    alert(item);
+
   }
 
+
+  addItem() {
+    let prompt = this.alertCtrl.create({
+      title: '请输入添加内容',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: '取消',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: '保存',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
